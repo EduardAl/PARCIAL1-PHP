d9db263862cd1b3ee6ff8e4db910856afa79a361
Eduardo <eduard_alfons@hotmail.com>
2018-08-23 23:53:13 -0600

    er

--- Ejercicio2/Parcial_ejercicio2.php
+++ Ejercicio2/Parcial_ejercicio2.php
@@ -1,34 +1,43 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
 	<meta charset="utf-8">
-	<title>Valores Numéricos</title>
+	<title>Cálculos</title>
 </head>
 <body>
 	<?php
-		$Numeros = [];
+		$Nums = [];
 		for($i=0;$i<4;$i++){
 			for($j=0;$j<5;$j++){
-				$Numeros[$i][$j]=$_POST['Valores'.$i.$j];
+				$Nums[$i][$j]=$_POST['Valores'.$i.$j];
 			}
 		}
 
-		$Mayor=$Numeros[0][0];
-		$PosicionX=0;$PosicionY=0;
+		$Mayor=$Nums[0][0];
+		$PX=0;$PY=0;
 		for($i=0;$i<4;$i++){
 			echo "<TR ALIGN='center'>";
 			echo "<TD BGCOLOR='cyan'>$i</TD>";
 			for($j=0;$j<5;$j++){
-				echo "<TD>".$Numeros[$i][$j]."</TD>";
-				if($Numeros[$i][$j]>$Mayor){
-					$Mayor=$Numeros[$i][$j];
-					$PosicionY=$j;
-					$PosicionX=$i;
+				echo "<TD>".$Nums[$i][$j]."</TD>";
+				if($Nums[$i][$j]>$Mayor){
+					$Mayor=$Nums[$i][$j];
+					$PY=$j;
+					$PX=$i;
 				}
 			}
 		}
 			echo "</TR>";
-			echo"<b>La posición con el primer elemento mayor ($Mayor) es: </b>[$PosicionX,$PosicionY]";
+			echo"<b>El valor mayor: ($Mayor) se encuentra en la posición: </b>[$PX,$Y]";
+			echo '<table border="1" cellpading="2" cellspacing="10">'
+			. '<tr align="center" bgcolor="blue">'
+			. '<td bgcolor="black"></td>'
+			. '<td>0</td>'
+			. '<td>1</td>'
+			. '<td>2</td>'
+			. '<td>3</td>'
+			. '<td>4</td>'
+			. '</tr>';
 	?>
 	</div>
 </body>
