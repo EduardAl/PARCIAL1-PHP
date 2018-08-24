848e9ab07b95dd7e3dcfcd82a5f0615c3ff56cfb
Eduardo <eduard_alfons@hotmail.com>
2018-08-24 01:33:21 -0600

    adsd

--- Ejercicio4/calculos.php
+++ Ejercicio4/calculos.php
@@ -7,17 +7,17 @@
 <body>
 	<?php
 		$Nums = [];
-		for($i=0;$i<4;$i++){
-			for($j=0;$j<5;$j++){
+		for($i=0;$i<10;$i++){
+			for($j=0;$j<10;$j++){
 				$Nums[$i][$j]=(rand(-900 , 900));
 			}
 		}
 		echo '<table border="1" cellpading="5" cellspacing="20"><tr align="center" bgcolor="gray"><td bgcolor="black"></td><td width="50px" height="50px">0</td><td width="50px" height="50px">1</td><td width="50px" height="50px">2</td><td width="50px" height="50px">3</td><td width="50px" height="50px">4</td><td width="50px" height="50px">5</td><td width="50px" height="50px">6</td><td width="50px" height="50px">7</td><td width="50px" height="50px">8</td><td width="50px" height="50px">9</td></tr>';
 		$Mayor=$Nums[0][0];
 		$PX=0;$PY=0;
-		for($i=0;$i<4;$i++){
+		for($i=0;$i<10;$i++){
 			echo "<TR ALIGN='center'><TD width='50px' height='50px' BGCOLOR='orange'>$i</TD>";
-			for($j=0;$j<5;$j++){
+			for($j=0;$j<10;$j++){
 				echo "<TD width='40px' height='40px' >".$Nums[$i][$j]."</TD>";
 				if($Nums[$i][$j]>$Mayor){
 					$Mayor=$Nums[$i][$j];
