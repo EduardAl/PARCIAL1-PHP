9087a57284d32b064d3c52316aa8995f1e40984f
Eduardo <eduard_alfons@hotmail.com>
2018-08-24 01:32:36 -0600

    aaewe

--- Ejercicio4/calculos.php
+++ Ejercicio4/calculos.php
@@ -7,38 +7,26 @@
 <body>
 	<?php
 		$Nums = [];
-		for($i=0;$i<10;$i++){
-			for($j=0;$j<10;$j++){
+		for($i=0;$i<4;$i++){
+			for($j=0;$j<5;$j++){
 				$Nums[$i][$j]=(rand(-900 , 900));
 			}
 		}
-		echo '<table border="1" cellpading="5" cellspacing="20"><tr align="center" bgcolor="gray"><td bgcolor="black"></td><td width="60px" height="60px">0</td><td width="60px" height="60px">1</td><td width="60px" height="60px">2</td><td width="60px" height="60px">3</td><td width="60px" height="60px">4</td></tr>';
-        $Mayor=$Nums[0][0];
-        $Mayor_fila=[];
-        $Mayor_fila[0]=$Nums[0][0];
-        $Mayor_col=[];
-        $Mayor_col[0]=$Nums[0][0];
-        $suma_filas[];
-        $suma_col[]=0;
+		echo '<table border="1" cellpading="5" cellspacing="20"><tr align="center" bgcolor="gray"><td bgcolor="black"></td><td width="50px" height="50px">0</td><td width="50px" height="50px">1</td><td width="50px" height="50px">2</td><td width="50px" height="50px">3</td><td width="50px" height="50px">4</td><td width="50px" height="50px">5</td><td width="50px" height="50px">6</td><td width="50px" height="50px">7</td><td width="50px" height="50px">8</td><td width="50px" height="50px">9</td></tr>';
+		$Mayor=$Nums[0][0];
 		$PX=0;$PY=0;
-		for($i=0;$i<10;$i++){
-            echo "<TR ALIGN='center'><TD width='60px' height='60px' BGCOLOR='orange'>$i</TD>";
-			for($j=0;$j<10;$j++){
-                echo "<TD width='40px' height='40px' >".$Nums[$i][$j]."</TD>";
-                if($Nums[$i][$j]>$Mayor_col[$i])
-                {
-                    $Mayor_col=$Nums[$i][$j];
-                    $suma_col+=$Nums[$i][$j];
-                }
-                if($Nums[$i][$j]>$Mayor)
-                {
-                    $Mayor=$Nums[$i][$j];
+		for($i=0;$i<4;$i++){
+			echo "<TR ALIGN='center'><TD width='50px' height='50px' BGCOLOR='orange'>$i</TD>";
+			for($j=0;$j<5;$j++){
+				echo "<TD width='40px' height='40px' >".$Nums[$i][$j]."</TD>";
+				if($Nums[$i][$j]>$Mayor){
+					$Mayor=$Nums[$i][$j];
 					$PY=$j;
 					$PX=$i;
 				}
 			}
 		}
-			echo "</TR><b>El valor mayor de la matriz: ($Mayor)";
+			echo "</TR><b>El valor mayor: ($Mayor) se encuentra en la posición: </b>[$PX,$PY]";
 	?>
 	</div>
 </body>
