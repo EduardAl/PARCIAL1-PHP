77caf5b26cb58f2e3149016f5a064c534a3cbef5
Eduardo <eduard_alfons@hotmail.com>
2018-08-24 01:44:42 -0600

    aa

--- Ejercicio4/calculos.php
+++ Ejercicio4/calculos.php
@@ -34,10 +34,10 @@
                 }
 			}
 		}
-            echo "</TR><b>El valor mayor: ($Mayor) se encuentra en la posición: </b>[$PX,$PY]";
+            echo "</TR><b>El valor mayor: ($Mayor) se encuentra en la posición: </b><br>[$PX,$PY]";
             for($i=0;$i<10;$i++)
             {
-                echo "</TR><b>El valor mayor de la fila ($i) es: </b>"+$Mayor_fila[$i];
+                echo "</TR><b>El valor mayor de la fila ($i) es: </b><br>"+$Mayor_fila[$i];
             }
 	?>
 	</div>
